235c3897b081094d5ce25f03e92b8eb5be173242
shovidhyan <shovidhyan77@gmail.com>
2025-11-25 19:05:22 +0530

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-   base: "/omega-care-coimbatore/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/omega-care-coimbatore/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+});
